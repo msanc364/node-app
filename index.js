45313f8779c900f9b101bb3d1f71cb1bfc747472
Michael Sanchez <msanc364@stu.southtexascollege.edu>
2024-10-28 23:36:09 -0500

    Hello world fetch is working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import route from "./routes/employeeRoute.js"
 
 const app = express()
 app.use(bodyParser.json())
@@ -20,4 +21,6 @@ mongoose.connect(MONGOURL).then( ()=> {
         
     })
 
-}).catch(error => console.log(error))
+}).catch( error => console.log(error))
+
+app.use("/api/employees", route)
