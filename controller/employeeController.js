301ac27a8f5147bf052006281bb76c3c261c7e6c
Michael Sanchez <msanc364@stu.southtexascollege.edu>
2024-11-07 18:37:22 -0600

    Fetch Employees is working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -5,7 +5,7 @@ export const create = async(req, res) => {
         const employeeData = new Employee(req.body)
         const { userName } = employeeData
         const employeeExist = await Employee.findOne({userName})
-        
+
         if(employeeExist) {
             return res.status(200).json({message: "Employee already exists."})
         }
@@ -21,14 +21,17 @@ export const create = async(req, res) => {
 
 
 
-
-
-
-
-
 export const fetch = async(req, res) => {
     try {
-        res.json("Hello World")
+
+        //res.json("Hello World")
+        const employees = await Employee.find()
+        if(employees.length === 0) {
+            return res.status(404).json({message: "Employees not found!"})
+        }
+
+
+        res.status(200).json(employees)
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
     }
