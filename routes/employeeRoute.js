1f32db303fc7f38a03ba405eee420963bcb39ff0
Michael Sanchez <msanc364@stu.southtexascollege.edu>
2024-11-07 18:52:22 -0600

    Update is working!

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {create, fetch} from "../controller/employeeController.js"
+import {create, fetch, update} from "../controller/employeeController.js"
 
 const route = express.Router()
 
@@ -7,5 +7,7 @@ const route = express.Router()
 
 route.post("/create", create)
 route.get("/fetch", fetch)
+route.put("/update/:id", update)
+
 
 export default route
